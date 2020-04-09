38d3035e52a9b1cd4ecb0a04546c31ee4618a389
Aaron Iemma <Shappiro@users.noreply.github.com>
2020-04-09 12:49:24 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
-# Add any additional setup tasks here
-chmod 600 /etc/ssl/private/ssl-cert-snakeoil.key
 
 # These tasks are run as root
 source /env-data.sh
-
 
 # Restrict subnet to docker private network
 echo "host    all             all             172.0.0.0/8               md5" >> $ROOT_CONF/pg_hba.conf
@@ -13,7 +10,6 @@ echo "host    all             all             192.168.0.0/16               md5" 
 # Listen on all ip addresses
 echo "listen_addresses = '*'" >> $CONF
 echo "port = ${POSTGRES_PORT}" >> $CONF
-
 
 # Create backup template for conf
 cat $CONF > $CONF.template
