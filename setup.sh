2e52cd01cdb0ee028585e5acec142180e8903458
ranjanprj <ranjanprj@users.noreply.github.com>
2023-05-04 13:18:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,9 @@ source /env-data.sh
 echo "host    all             all             172.0.0.0/8               md5" >> $ROOT_CONF/pg_hba.conf
 # And allow access from DockerToolbox / Boottodocker on OSX
 echo "host    all             all             192.168.0.0/16               md5" >> $ROOT_CONF/pg_hba.conf
+
+echo "host    all             all             0.0.0.0/0            trust" >> $ROOT_CONF/pg_hba.conf
+
 # Listen on all ip addresses
 echo "listen_addresses = '*'" >> $CONF
 echo "port = ${POSTGRES_PORT}" >> $CONF
